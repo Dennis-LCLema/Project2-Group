1c4591d17af63ed85254331090ef09ec5be1167f
arm <adrianmoravec@Adrians-MacBook-Pro.local>
2019-05-15 13:13:11 -0500

    hopefully got signin working and persistent as well as logout

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,44 +1,52 @@
 var db = require("../models");
 var path = require("path");
 
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
+
+     // If the user already has an account send them to the members page
+     if (req.user) {
+      res.redirect("/home");
+    }
     res.sendFile(path.join(__dirname, "../views/index.html"));
 
-    // db.Example.findAll({}).then(function(dbExamples) {
-    //   res.render("index", {
-    //     msg: "Welcome!",
-    //     examples: dbExamples
-    //   });
-    // });
   });
 
-  app.get("/mlb", function(req, res) {
+    // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/home", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/home.html"));
+  });
+
+  app.get("/mlb", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../views/mlb.html"))
   });
 
-  app.get("/nba", function(req, res) {
+  app.get("/nba", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../views/nba.html"))
   });
 
-  app.get("/nfl", function(req, res) {
+  app.get("/nfl", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../views/nfl.html"))
   });
 
-  app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/home.html"))
-  });
+  // app.get("/home", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../views/home.html"))
+  // });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  // // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
  app.get("*", function(req, res) {
