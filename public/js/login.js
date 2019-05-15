17f24333eb8b0641c6332ee0e994d4c248704f56
Dennis Campoverde <dennyslema441@gmail.com>
2019-05-14 19:03:18 -0500

    added a log in function

--- public/js/login.js
+++ public/js/login.js
@@ -18,25 +18,30 @@
 // 	response.sendFile(path.join(__dirname + '/login.html'));
 // });
 
-// app.post('/auth', function(request, response) {
-// 	var username = request.body.username;
-// 	var password = request.body.password;
-// 	if (username && password) {
-// 		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-// 			if (results.length > 0) {
-// 				request.session.loggedin = true;
-// 				request.session.username = username;
-// 				response.redirect('/home');
-// 			} else {
-// 				response.send('Incorrect Username and/or Password!');
-// 			}			
-// 			response.end();
-// 		});
-// 	} else {
-// 		response.send('Please enter Username and Password!');
-// 		response.end();
-// 	}
-// });
+app.post('/auth', function(request, response) {
+	$("#loginBtn").on("click", function () {
+		var username = request.body.username;
+		var password = request.body.password;
+		if (username && password) {
+			connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results) {
+				if (results.length > 0) {
+					request.session.loggedin = true;
+					request.session.username = username;
+					response.redirect('/home');
+				} else {
+					response.send('Incorrect Username and/or Password!');
+				}			
+				response.end();
+			});
+			} else {
+			response.send('Please enter Username and Password!');
+			response.end();
+		}
+	});
+});
+
+
+     
 
 // app.get('/home', function(request, response) {
 // 	if (request.session.loggedin) {
@@ -54,29 +59,7 @@
 
 
 
-// $(".login-form").hide();
-// $(".login").css("background", "none");
 
-// $(".login").click(function(){
-//   $(".signup-form").hide();
-//   $(".login-form").show();
-//   $(".signup").css("background", "none");
-//   $(".login").css("background", "rgb(99, 99, 99)");
-// console.log ("click on login")
-// });
-
-
-// $(".signup").click(function(){
-//   $(".signup-form").show();
-//   $(".login-form").hide();
-//   $(".login").css("background", "none");
-//   $(".signup").css("background", "rgb(99, 99, 99)");
-// console.log("click on sign in")
-// });
-
-// $(".btn").click(function(){
-//   $(".input").val("");
-// });
 
 $(document).ready(function () {
 
